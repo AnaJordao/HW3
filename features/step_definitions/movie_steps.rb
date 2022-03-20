a070463323fb2695aeb99b4faafbe64b925a153f
AnaJordao <acjpjvjp@gmail.com>
2022-03-20 16:52:31 -0300

    check e uncheked ratings

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,3 +4,15 @@ Given /the following movies exist:/ do |dados|  # dados será a tabela de filmes
         Movie.create(:title => linha[:title], :release_date => linha[:release_date], :rating => linha[:rating])
     end
 end
+
+When /I check the following ratings: (.*)/ do |checked|
+    checked.split(', ').each do |rating|
+        check("ratings_#{rating}")
+    end
+end
+
+When /I uncheck the following ratings: (.*)/ do |unchecked|
+    unchecked.split(', ').each do |rating|
+        uncheck("ratings_#{rating}")
+    end
+end
