717d43820a0f75b38a7d50fb6d77687f9d6c9ebd
AnaJordao <acjpjvjp@gmail.com>
2022-03-21 17:42:43 -0300

    Comentado

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,25 +1,25 @@
 Given /the following movies exist:/ do |dados|  # dados será a tabela de filmes
-    @linhas_totais = 0
+    @linhas_totais = 0                          
     dados.hashes.each do |linha|                # transforma dados num hash em q a chave é o header da tabela
         # usa o create do ActiveRecord do model Movie
         Movie.create(:title => linha[:title], :release_date => linha[:release_date], :rating => linha[:rating])
-        @linhas_totais += 1
+        @linhas_totais += 1                     # conta as linhas da tabela
     end
 end
 
-When /I check the following ratings: (.*)/ do |checked|
-    checked.split(', ').each do |rating|
-        check("ratings_#{rating}")
+When /I check the following ratings: (.*)/ do |checked| # pega a lista de ratings como uma string
+    checked.split(', ').each do |rating|                # divide a string em cada rating
+        check("ratings_#{rating}")                      # seleciona as ratings usando o id do haml
     end
 end
 
-When /I uncheck the following ratings: (.*)/ do |unchecked|
-    unchecked.split(', ').each do |rating|
-        uncheck("ratings_#{rating}")
+When /I uncheck the following ratings: (.*)/ do |unchecked|# pega a lista de ratings como uma string
+    unchecked.split(', ').each do |rating|                  # divide a string em cada rating
+        uncheck("ratings_#{rating}")                        # uncheck as ratings usando o id do haml
     end
 end
 
-Then /I should see all of the movies/ do
+Then /I should see all of the movies/ do    # checa se a pagina tem o numero de table rows igual a contagem da tabela a cima
     if page.respond_to? :should
       page.should has_css("table#movies tbody tr", :count => @linhas_totais)
     else
@@ -27,7 +27,7 @@ Then /I should see all of the movies/ do
     end
 end
 
-When /I check all ratings/ do 
+When /I check all ratings/ do # o mesmo que o "I should see all of the movies", pois o comportamento é o mesmo
     if page.respond_to? :should
         page.should has_css("table#movies tbody tr", :count => @linhas_totais)
     else
@@ -36,8 +36,8 @@ When /I check all ratings/ do
 end
 
 Then /I should see "(.*)" before "(.*)"/ do |first, second|
-    first = page.body.index(first)
-    second = page.body.index(second)
+    first = page.body.index(first)      # pega o index do primeiro filme no body recebido
+    second = page.body.index(second)    # pega o index do segundo filme no body recebido
 
-    assert(first < second)
-end
+    assert(first < second)              # checa se o primeiro tem um indice menor que                                   
+end                                     # o segundo e portanto vem antes
